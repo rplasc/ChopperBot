ccb156ec9a36994a10f417f5abdbda6193d0e970
Raul <148407066+rplasc@users.noreply.github.com>
2023-10-29 09:24:59 -0700

    Update openai_util.py

--- utils/openai_util.py
+++ utils/openai_util.py
@@ -1,6 +1,5 @@
 from base64 import b64decode
-from os import path
-import os
+from os import path, remove, makedirs
 from pathlib import Path
 import discord
 import openai
@@ -8,9 +7,8 @@ import aiohttp
 import json
 from src.aclient import client
 
-openai.api_key = client.openAI_API_key # Add your OpenAI API key here
+openai.api_key = client.openAI_API_key
 
-# Gets respone for Chatgpt model
 async def get_openai_response(messages):
     async with aiohttp.ClientSession() as session:
         async with session.post(
@@ -20,44 +18,45 @@ async def get_openai_response(messages):
         ) as response:
             data = await response.json()
             return data['choices'][0]['message']['content']
-        
-# Generates image and returns url
-async def generate_img(description):
-   DATA_DIR = Path.cwd() / "responses" # Create directory for responses
-   DATA_DIR.mkdir(exist_ok=True)
-    
-   response = openai.Image.create(
-        model="image-alpha-001",  # Use the appropriate DALL·E model ID
-        prompt=description,
-        n=1,  # Number of images to generate
-        size="256x256",  # Image resolution
-        response_format="b64_json"  # Get the image as a URL
-        )
-   
-   file_name = DATA_DIR / f"{description[:5]}-{response['created']}.json"
-   with open(file_name, mode="w", encoding="utf-8") as file:
-        json.dump(response, file)
-        
-   path = await conversion(file_name)
-   return path
 
-# Converts generated image to png
-async def conversion(path):
+
+async def generate_img(description):
     DATA_DIR = Path.cwd() / "responses"
-    JSON_FILE = DATA_DIR / path
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+    
+    response = openai.Image.create(
+        model="image-alpha-001",
+        prompt=description,
+        n=1,
+        size="256x256",
+        response_format="b64_json"
+    )
+
+    file_name = f"{description[:5]}-{response['created']}.json"
+    file_path = DATA_DIR / file_name
+
+    with open(file_path, mode="w", encoding="utf-8") as file:
+        json.dump(response, file)
+
+    return await conversion(file_path)
+
+async def conversion(json_path):
+    JSON_FILE = Path(json_path)
     IMAGE_DIR = Path.cwd() / "images"
     IMAGE_DIR.mkdir(parents=True, exist_ok=True)
 
     with open(JSON_FILE, mode="r", encoding="utf-8") as file:
         response = json.load(file)
 
-    image_file = []
-    for index, image_dict in enumerate(response["data"]):
-        image_data = b64decode(image_dict["b64_json"])
-        image_file = IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png"
-        with open(image_file, mode="wb") as png:
-            png.write(image_data)
-            
-    os.remove(path)
+    # Assuming you only want the first image
+    image_dict = response["data"][0]
+    image_data = b64decode(image_dict["b64_json"])
+    image_path = IMAGE_DIR / f"{JSON_FILE.stem}.png"
 
-    return image_file
+    with open(image_path, mode="wb") as png:
+        png.write(image_data)
+
+    # Remove JSON file after conversion
+    remove(JSON_FILE)
+
+    return image_path
